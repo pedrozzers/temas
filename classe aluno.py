34c9932bc55acb6a479566c93ba5653fd2ab9f6b
pedrozzers <102078923+pedrozzers@users.noreply.github.com>
2022-12-11 18:27:44 -0300

    Add files via upload

---
+++ classe aluno.py
@@ -0,0 +1,7 @@
+class aluno:
+    def _init_(self,série,disciplinas,dificuldades,tempo):
+        self.série=série
+        self.disciplinas=disciplinas
+        self.dificuldades=dificuldades
+        self.tempo=tempo
+        

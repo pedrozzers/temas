70b14af8c843978a5fd0877052fb1985e825748c
pedrozzers <102078923+pedrozzers@users.noreply.github.com>
2022-12-11 18:49:06 -0300

    Add files via upload

---
+++ Inicio pokemon.py
@@ -0,0 +1,13 @@
+import csv
+
+
+csv
+
+import pandas as pd
+arquivo=pd.read_csv("MICRODADOS_ENEM_ESCOLA.csv", escoding="ANSI", sep=";")
+
+arquivo.columns
+arquivo["Name"]
+arquivo['Name'] [o:10]
+
+pokemon.sort_values(["HP","Atrack"],ascending=[True,False])

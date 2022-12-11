eafe5038de6ed9724a11154fe4a6a8885e5f34be
pedrozzers <102078923+pedrozzers@users.noreply.github.com>
2022-12-11 20:17:00 -0300

    Add files via upload

---
+++ contar letras at8.py
@@ -0,0 +1,6 @@
+#Código para contar a quantidade de letras em uma palavra (usada no twitter por exemplo)
+
+word = str(input("Digite uma palavra: "))
+lista = list(word)
+print(f"A palavra {word} tem {len(word)} letras")
+
